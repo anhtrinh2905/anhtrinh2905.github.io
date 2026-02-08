e1aedfa76f23adcfb39fd00a0d70083121c7f1a5
Trinh Anh <anh.trinh.ext@gitguardian.com>
2026-02-08 14:49:18 +0700

    chore: change Croll Hint to component for reusing

--- src/app/(site)/home/page.tsx
+++ src/app/(site)/home/page.tsx
@@ -7,6 +7,7 @@ import Stack from "@/components/home/CardRotate";
 import CardWhatICanDo from "@/components/home/CardWhatIcanDo";
 import { WHATICANDO_INFO } from "@/lib/homePageInfo";
 import PageTransition from "@/components/PageTransition";
+import CrollHint from "@/components/CrollHint";
 
 
 export default function Home() {
@@ -93,35 +94,7 @@ export default function Home() {
 
             {/* Scroll hint */}
             <div className="w-full flex justify-center mt-10">
-            <a
-                href="#what-i-can-do"
-                aria-label="Scroll down to What I can do"
-                className="
-                group
-                flex h-12 w-12 items-center justify-center
-                rounded-full
-                border border-white/20
-                bg-white/5 backdrop-blur-sm
-                shadow-[0_10px_30px_rgba(0,0,0,0.35)]
-                transition
-                hover:bg-white/10 hover:-translate-y-0.5
-                active:scale-95
-                animate-bounce
-                "
-            >
-                <svg
-                viewBox="0 0 24 24"
-                className="h-6 w-6 text-white/80 transition group-hover:text-white"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                >
-                <path d="M12 5v14" />
-                <path d="M19 12l-7 7-7-7" />
-                </svg>
-            </a>
+                <CrollHint id='what-i-can-do' />
             </div>
 
 
---
+++ src/components/CrollHint.tsx
@@ -0,0 +1,42 @@
+interface CrollHintProps {
+    id: string;
+}
+
+export default function CrollHint({
+    id = '',
+}: CrollHintProps){
+    const linkToSection = "#" + id;
+    return(
+        <div>
+            <a
+                href={linkToSection}
+                aria-label="Scroll down to What I can do"
+                className="
+                group
+                flex h-12 w-12 items-center justify-center
+                rounded-full
+                border border-white/20
+                bg-white/5 backdrop-blur-sm
+                shadow-[0_10px_30px_rgba(0,0,0,0.35)]
+                transition
+                hover:bg-white/10 hover:-translate-y-0.5
+                active:scale-95
+                animate-bounce
+                "
+            >
+                <svg
+                viewBox="0 0 24 24"
+                className="h-6 w-6 text-white/80 transition group-hover:text-white"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                >
+                <path d="M12 5v14" />
+                <path d="M19 12l-7 7-7-7" />
+                </svg>
+            </a>
+        </div>
+    )
+}
