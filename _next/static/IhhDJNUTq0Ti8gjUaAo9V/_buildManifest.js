f883371794eee3d8605ae8e894a0cf2a03226d95
Trinh Anh <anh.trinh.ext@gitguardian.com>
2026-02-13 15:32:12 +0700

    Updates

--- _next/static/IhhDJNUTq0Ti8gjUaAo9V/_buildManifest.js
+++ _next/static/IhhDJNUTq0Ti8gjUaAo9V/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/anhtrinh2905.github.io//_next/:path+",
-        "destination": "/anhtrinh2905.github.io/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
