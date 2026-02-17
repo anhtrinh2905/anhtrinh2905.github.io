447f606880cef1d5e0ed110a91abd61246281284
Trinh Anh <anh.trinh.ext@gitguardian.com>
2026-02-18 00:08:25 +0700

    feat: Add snowfall effect

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,12 +1,16 @@
+'use client'
+
 import BubbleMenu from "@/components/BubbleMenu";
 import { Footer } from "@/components/footer/Footer";
 import { getGreeting } from "@/lib/greeting";
+import Snowfall from "react-snowfall";
 
 export default function App() {
   const greeting = getGreeting();
 
   return (
     <div className="min-h-screen flex flex-col">
+      <Snowfall />
       <main className="flex-1">
         <BubbleMenu logo="cat_logo.svg" greeting={greeting} />
       </main>
--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,12 +1,16 @@
+'use client'
+
 import { Footer } from "@/components/footer/Footer";
 import { Header } from "@/components/header/Header";
 import PageTransition from "@/components/PageTransition";
 import ScrollToTop from "@/components/ScrollToTop";
 import ".././globals.css";
+import Snowfall from "react-snowfall";
 
 export default function SiteLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Snowfall />
       <Header />
       <PageTransition>{children}</PageTransition>
       <ScrollToTop />
